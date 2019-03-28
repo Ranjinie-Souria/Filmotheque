6edabd503e132d28044bbebff16798fbea314718
Ranjinie <ranjinie.souria@gmail.com>
2019-03-28 13:58:11 +0100

    Page d'accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" style="width: 100px;"><br/>
+    <a style="font-size: 8px" href="https://www.flaticon.com/free-icon/movie_125345">source du logo</a>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
