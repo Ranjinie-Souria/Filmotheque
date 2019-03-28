974ca2dbd9abc440518c3d778f584fe344d469fb
Ranjinie <ranjinie.souria@gmail.com>
2019-03-28 14:25:59 +0100

    Recherche

---
+++ src/components/Recherche.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <h1>Ma filmothèque</h1>
+    <p>
+      Bienvenue sur la filmothèque. Vous pouvez rechercher un film.
+    </p>
+
+    <br />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="12">
+          <div v-if="alert"><b-alert show variant="danger">
+            Aucun film trouvé !
+          </b-alert></div>
+        </b-col>
+        <b-col sm="12">
+          <b-card class="text-center">
+            <b-card-title style="max-width: 20rem;" class="mb-2">
+              Recherche d'un film
+            </b-card-title>
+            <b-card-text>
+              <b-form @submit.prevent="submit">
+                <b-form-input
+                        v-model="title"
+                        placeholder="Entrez le nom du film"
+                        required
+                />
+                <br />
+                <b-button type="submit" variant="primary">Chercher</b-button>
+              </b-form>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+
+
+
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        title: "",
+        alert: false
+      };
+    },
+  methods:{
+    submit() {
+      if (!isNaN(this.title)) {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c4183e64dc74d13d605f6815173449f3&query=${this.title}`)
+                .then((response) => {
+                  if (response.data.status) {
+                    this.alert = false;
+                    console.log(response.data.product);
+                    this.title = "";
+                  } else {
+                    this.alert = true;
+                  }
+                })
+      }
+    }
+
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" style="width: 100px;"><br/>
     <a style="font-size: 8px" href="https://www.flaticon.com/free-icon/movie_125345">source du logo</a>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Recherche/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Recherche from '@/components/Recherche.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Recherche
   }
 }
 </script>
