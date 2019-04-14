fb369752b8ce150ac68d048c7d464b5dafcaf7cb
SebTex <sebastien.tex6@hotmail.fr>
2019-04-14 23:44:21 +0200

    Ajout des tests unitaire de FicheFilm

--- tests/unit/FicheFilm.spec.js
+++ tests/unit/FicheFilm.spec.js
@@ -1,5 +1,26 @@
-import FicheFilm from '@/models/FicheFilms.js'
+import FicheFilm from '@/models/FicheFilm.js'
 
-test('Exemple test cas général', () => {
-
-})
+test('Exemple test cas général de FicheFilm', () => {
+    const film = new FicheFilm("avatar", "/1sd5g3g486gs1.png", "2019-01-01", "Steven Spielberg", "Pedro le rigolo");
+    expect(film.nom).toMatch('avatar');
+    expect(film.affiche).toMatch('/1sd5g3g486gs1.png');
+    expect(film.anneSortie).toMatch('2019-01-01');
+    expect(film.realisateur).toMatch('Steven Spielberg');
+    expect(film.acteurPrincipaux).toMatch('Pedro le rigolo');
+});
+test('Exemple où la FicheFilm est vide', () => {
+    const film = new FicheFilm("", "", "", "", "");
+    expect(film.nom).toMatch('');
+    expect(film.affiche).toMatch('');
+    expect(film.anneSortie).toMatch('');
+    expect(film.realisateur).toMatch('');
+    expect(film.acteurPrincipaux).toMatch('');
+});
+test('Exemple avec 1 element demandé dans la FicheFilm', () => {
+    const film = new FicheFilm("avatar", "", "2019-01-01", "Steven Spielberg", "Pedro le rigolo");
+    expect(film.nom).toMatch('avatar');
+    expect(film.affiche).toMatch('');
+    expect(film.anneSortie).toMatch('2019-01-01');
+    expect(film.realisateur).toMatch('Steven Spielberg');
+    expect(film.acteurPrincipaux).toMatch('Pedro le rigolo');
+});
